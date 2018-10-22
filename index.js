9fa5794ceeedbd193661e81473ff10eb44955b7d
ty000 <ty000@users.noreply.github.com>
2018-10-22 09:57:56 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const { get, isFunction, bind, noop } = require('lodash');
+const redis = require('redis');
+const redisCommands = require('redis-commands');
+const { promisify } = require('util');
+
+const clients = {};
+
+module.exports = {
+  getClient,
+};
+
+function getClient(clientName = 'base', { onError = noop, promisify = false } = {}) {
+  return get(clients, clientName, initRedisClient(promisify, onError));
+}
+
+function initRedisClient(promisify, onError) {
+  const client = redis.createClient();
+  client.on('error', onError);
+  if (promisify)
+    return decorateClient(client);
+  return client;
+}
+
+function decorateClient(client) {
+  for (const propName in client) {
+    if (isFunction(client[propName]))
+      client[propName] = decorateMethod(client, client[propName], propName);
+  }
+  return client;
+}
+
+function decorateMethod(client, method, name) {
+  if (redisCommands.exists(name))
+    return bind(promisify(method), client);
+  return method;
+}
